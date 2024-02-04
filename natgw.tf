6f13e8a5dc1caf28cac726d980d748909f6c25c1
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2024-02-04 18:31:47 +0530

    Fixed docs

--- natgw.tf
+++ natgw.tf
@@ -5,6 +5,6 @@ resource "aws_nat_gateway" "natgw" {
   tags = {
     Name = "gw NAT"
   }
-  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # To ensure proper ordering, it is recommended to add an explicit dependency on internet gateway for VPC
   depends_on = [aws_internet_gateway.gw]
 }
